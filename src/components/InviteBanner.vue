65891663b7f74905ac3faf50b562950e09e14ceb
Sam Huber <mail.lucky5sam@gmail.com>
2026-08-29 15:28:29 -0400

    invite banner cleanup

--- src/components/InviteBanner.vue
+++ src/components/InviteBanner.vue
@@ -114,7 +114,7 @@ onUnmounted(() => {
       class="relative flex flex-col-reverse items-start gap-6 overflow-hidden rounded-lg border border-border-subtle bg-surface-default p-6 sm:flex-row sm:justify-between"
     >
       <!-- Ambient fire glow flush to the bottom edge (decorative, non-interactive). -->
-      <FireGlow position="absolute" :z-index="0" :height="90" :ember-count="8" />
+      <FireGlow position="absolute" :z-index="20" :height="90" :ember-count="8" />
 
       <!-- Left content -->
       <div class="relative z-10 min-w-0 flex-1">
@@ -163,7 +163,7 @@ onUnmounted(() => {
                 d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
               />
             </svg>
-            Copy Invite Link
+            Invite Link
           </BaseButton>
           <button
             v-if="leagueCode"
